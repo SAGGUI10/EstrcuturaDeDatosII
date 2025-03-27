e3099fe40482817b7736ae3a0d3196e06426b0b7
SAGGUI <emmanuelsantiago692@gmail.com>
2025-03-27 10:05:12 -0500

    Subiendo colas

---
+++ colas/ejemplo1.php
@@ -0,0 +1,20 @@
+<?php
+    $cola = [];
+
+    array_push($cola,"Orden 1");
+    array_push($cola,"Orden 2");
+    array_push($cola,"Orden 3");
+    array_push($cola,"Orden 4");
+    array_push($cola,"Orden 5");
+    array_push($cola,"Orden 6");
+    array_push($cola,"Orden 7");
+    array_push($cola,"Orden 8");
+    array_push($cola,"Orden 9");
+
+    $oreden = array_shift($cola);
+
+    echo "La primera orden es: ".$oreden;
+
+
+
+?>
