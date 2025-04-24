d220d1726af9891b719adb335f3ae6d2d5ec490d
SAGGUI <emmanuelsantiago692@gmail.com>
2025-04-24 08:10:24 -0500

    ingresa lista enlazada

---
+++ Arrays/parcial.php
@@ -0,0 +1,11 @@
+<?php
+    $juegos = array("HALO","CALL OF DUTY","FIFA","MINECRAFT");
+    $elemto1 = array_pop($juegos);
+    $elemto2 = array_pop($juegos);
+    $elemto3 = array_pop($juegos);
+
+    $juegos2 = array($elemto1,$elemto2,$elemto3);
+    print_r($juegos2);
+    
+
+?>
